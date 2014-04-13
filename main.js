974929b272831e4c1e6f10d6bd0654644628b70e
Alexandre Servian <alexandreservian@gmail.com>
2014-04-13 18:46:18 -0300

    Add POST,PUT AND DELETE

--- main.js
+++ main.js
@@ -16,7 +16,30 @@ var xhr = (function(){
 		module._onready(ajax,callback);
 		ajax.send();
 	};
+	module.post=function(url,data,callback){
+		var ajax=module._init();
+		ajax.open('POST', url);
+		module._onready(ajax,callback);
+		ajax.setRequestHeader("Content-Type", "application/json");
+		ajax.send(data);
+	},
+	module.put=function(url,data,callback){
+		var ajax=module._init();
+		ajax.open('PUT', url);
+		module._onready(ajax,callback);
+		ajax.setRequestHeader("Content-Type", "application/json");
+		ajax.send(data);
+	},
+	module.delete=function(url,callback){
+		var ajax=module._init();
+		ajax.open('DELETE',url);
+		module._onready(ajax,callback);
+		ajax.send();
+	};
 	return {
-		get: module.get
+		get: module.get,
+		post: module.post,
+		put: module.put,
+		delete: module.delete
 	};
 }());
